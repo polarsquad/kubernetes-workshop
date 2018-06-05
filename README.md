0696a6b00fa5e8d604a2e236aa96e650c2cd832b
Mikko <mikko@Mikkos-MBP-2.localdomain>
2018-06-05 10:44:16 +0300

    Adding application

--- README.md
+++ README.md
@@ -1,23 +1,4 @@
-# kubernetes-workshop
-This repository contains sources for Polar Squad [Kubernetes](https://kubernetes.io) workshop guide and material.
+# Apps
+This directory contains example applications for Istio workshop.
 
-Clone this repository (`git clone --recursive git://github.com/polarsquad/kubernetes-workshop.git`) and start following the guide at [https://polarsquad.github.io/kubernetes-workshop](https://polarsquad.github.io/kubernetes-workshop)
-
-## Development
-** NOTE: You don't need to do these steps, [read the guide!](https://polarsquad.github.io/kubernetes-workshop) These are only for developers who would like to run the guide locally.
-
-### Prerequisites
-- Clone this repository with submodules `git clone --recursive git://github.com/polarsquad/kubernetes-workshop.git`
-- Install Go
-- `make setup`
-
-### Serve
-To view the guide locally, run:
-```shell
-make serve
-```
-
-### Build
-```shell
-make build
-```
+You can find pre-built images from [Polar Squad Docker Hub](https://hub.docker.com/r/polarsquad/)
---
+++ apps/backend/app_v2.js
@@ -0,0 +1,76 @@
+const loki = require('lokijs');
+const express = require('express');
+const bodyParser = require('body-parser')
+
+// HTTP
+const app = express();
+
+// DB
+const db = new loki('loki.json');
+const stuff = db.addCollection('stuff');
+
+// Other
+const version = '2';
+
+function getByTitle(title) {
+    return stuff.find({title: title})[0];
+}
+
+function upsert(title, text) {
+    const item = getByTitle(title);
+    if (item) {
+        stuff.findAndUpdate({title: title}, function(o) {
+            o.text = text;
+            return o;
+        });
+    } else {
+        stuff.insert({title: title, text: text});
+    }
+}
+
+function getTitles() {
+    return stuff.find().map((o) => o.title);
+}
+
+function itemToResponse(item) {
+    return {
+        title: item.title,
+        text: item.text
+    };
+}
+
+app.get('/', function(req, res) {
+    res.json(getTitles());
+});
+
+app.get('/version', function(req, res) {
+    res.send(version);
+});
+
+app.get('/:title', function(req, res) {
+    const item = getByTitle(req.params.title);
+    if (item) {
+        res.json(itemToResponse(item));
+    } else {
+        res.status(404);
+        res.json({message: 'not found'})
+    }
+});
+
+app.post('/:title', bodyParser.text(), function(req, res) {
+    if (typeof req.body === 'string') {
+        upsert(req.params.title, req.body);
+        res.json({message: 'success'});
+    } else {
+        res.status(400);
+        res.json({message: 'missing text'});
+    }
+});
+
+app.listen(6000, function() {
+    console.log('Backend app listening on port 6000')
+});
+
+process.on('SIGINT', function() {
+    process.exit();
+});
